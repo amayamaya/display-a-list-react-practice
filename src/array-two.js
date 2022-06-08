7af8a4ff3256ad72aa56e3465f75ed5df17c67c6
‘AMAYA <aamaya547@gmail.com>
2022-06-08 14:53:33 -0700

    added fruit array

--- src/array-two.js
+++ src/array-two.js
@@ -1,1 +1,26 @@
-
+export const games = [
+  {
+    name: 'pineapple',
+    flavor: 'sour',
+    color: 'yellow & green',
+    origin: 'Brazil',
+  },
+  {
+    name: 'starfruit',
+    flavor: 'slightly sweet',
+    color: 'yellow',
+    origin: 'Sri Lanka, the Moluccas',
+  },
+  {
+    name: 'banana',
+    flavor: 'varies in sweetness',
+    color: 'yellow',
+    origin: 'Southeast Asia, India',
+  },
+  {
+    name: 'guava',
+    flavor: 'uniquely sweet',
+    color: 'yellow-green',
+    origin: 'South America, the Caribbean',
+  },
+];

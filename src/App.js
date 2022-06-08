cf15d0efd2881d93420f58ba4369d168c09ace60
‘AMAYA <aamaya547@gmail.com>
2022-06-08 12:15:38 -0700

    removed logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 // import your arrays here
 
 function App() {
-  return (
-    <div className="App">
-        Render all your lists here. Pass the arrays as props.
-    </div>
-  );
+  return <div className="App">Render all your lists here. Pass the arrays as props.</div>;
 }
 
 export default App;

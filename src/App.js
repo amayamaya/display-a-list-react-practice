1b2dcd4e3695dba5173926b990ed6c5aa0c94f3a
‘AMAYA <aamaya547@gmail.com>
2022-06-08 17:33:58 -0700

    SingersList appearing in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FlowersList from './FlowersList';
 import FruitList from './FruitList';
+import SingersList from './SingersList';
 import { flowers } from './flowers.js';
 import { fruit } from './fruit.js';
 
@@ -10,6 +11,7 @@ function App() {
       <div className="App">Render all your lists here. Pass the arrays as props.</div>
       <FlowersList flowers={flowers} />
       <FruitList fruit={fruit} />
+      <SingersList />
     </>
   );
 }
--- src/SingersItem.js
+++ src/SingersItem.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+export default function ArtistItem({ artist, genre, favorite, album }) {
+  return (
+    <div className="singers">
+      <p>{artist}</p>
+      <p>{genre}</p>
+      <p>{favorite}</p>
+    </div>
+  );
+}
+
+//when mapping in item map through albums.title list is the same
